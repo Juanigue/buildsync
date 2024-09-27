107385b50c0bce68fcce7eb10cfb0140b2eba37b
Juanigue <juaniguerrero2203@gmail.com>
2024-09-27 11:09:36 -0300

    Primera parte del sistema auth

--- BuildSync/App.tsx
+++ BuildSync/App.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { supabase } from "./utils/supabase";
 import Auth from "./components/Auth";
 import Account from "./components/Account";
@@ -7,7 +6,7 @@ import { View } from "react-native";
 import { Session } from "@supabase/supabase-js";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, Image } from "react-native";
-import Button from "./components/Button";
+
 export default function App() {
   const [session, setSession] = useState<Session | null>(null);
 
@@ -20,32 +19,24 @@ export default function App() {
       setSession(session);
     });
   }, []);
-  const handlePress = () => {
-    console.log("Button pressed");
-  };
 
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.textos}>BuildSync</Text>
+      <Image
+        source={require("./assets/logo1.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <StatusBar style="auto" />
       {session && session.user ? (
         <Account key={session.user.id} session={session} />
       ) : (
         <Auth />
       )}
-      <View style={styles.container}>
-        <Text style={styles.textos}>BuildSync</Text>
-        <Image
-          source={require("./assets/logo1.png")}
-          style={{ width: 200, height: 200 }}
-        />
-        <Button title="Press Me" onPress={handlePress} />
-        <Button title="Click Me" onPress={handleClick} />
-      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
